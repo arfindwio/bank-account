0483501a816a19f2d044920bbab2231df83803b6
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2023-08-24 18:52:47 +0700

    add alert

--- bank_account.js
+++ bank_account.js
@@ -10,6 +10,9 @@ const tambahSaldo = () => {
   if (menambahSaldo !== null) {
     saldo += parseInt(menambahSaldo);
     updateSaldoDisplay();
+    alert("Saldo berhasil ditambahkan");
+    console.log("Saldo berhasil ditambahkan");
+    console.log("Saldo anda :", saldo);
   }
 };
 
@@ -20,8 +23,12 @@ const kurangiSaldo = () => {
     if (saldo >= mengurangSaldo) {
       saldo -= mengurangSaldo;
       updateSaldoDisplay();
+      alert("Saldo berhasil dikurangi");
+      console.log("Saldo berhasil dikurangi");
+      console.log("Saldo anda :", saldo);
     } else {
-      console.log("Saldo tidak mencukupi.");
+      alert("Saldo gagal dikurangi karena saldo tidak mencukupi");
+      console.log("Saldo gagal dikurangi karena saldo tidak mencukupi");
     }
   }
 };
